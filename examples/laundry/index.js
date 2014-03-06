c22820c710043bfe28f5b569de376a627df116d2
Edward Look <edwlook@gmail.com>
2014-03-05 21:19:14 -0800

    Use error objects

--- examples/laundry/index.js
+++ examples/laundry/index.js
@@ -7,9 +7,9 @@ var casClient = require('../../lib').configure({
 });
 
 casClient.login(function(err, data) {
-  if (err) return console.log(err);
+  if (err) return console.log(err.message);
   casClient.get('https://csweb.housing.berkeley.edu/student/laundry/room_summary_srv.php', function(err, data) {
-    if (err) return console.log(err);
+    if (err) return console.log(err.message);
     console.log(data);
   });
 });
